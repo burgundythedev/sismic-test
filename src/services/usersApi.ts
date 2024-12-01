134abff92aabd69c3a61a9634e537cb6daed1eaf
burgundythedev <bourgogne.olivier@gmail.com>
2024-12-01 20:58:52 +0100

    feat: fixing responsive css

--- src/services/usersApi.ts
+++ src/services/usersApi.ts
@@ -1,6 +1,13 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import UserData from '../models';
 
+
+// Décision Technique pour mes requetes API:
+//Réduction du code : Moins de code à écrire grâce à l’automatisation des requêtes et mutations.
+//Gestion des états : RTK Query gère automatiquement les états de chargement, succès et échec.
+//Cache automatique : Réduit les requêtes réseau répétées et améliore les performances.
+//Optimisation : Mise à jour optimiste des données pour une expérience utilisateur plus fluide.
+
 export const usersApi = createApi({
   reducerPath: 'usersApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://6749ba9c8680202966325fec.mockapi.io/' }), 
